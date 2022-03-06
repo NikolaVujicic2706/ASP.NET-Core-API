c3c40aa25dba299ac86dbac8776a85a683eb1565
nick_without_wick <nikolavujicic.etf@gmail.com>
2022-03-06 18:57:07 +0100

    Model and mock data have been created

--- CityInfo/CityInfo.API/Controlers/CitiesControlers.cs
+++ CityInfo/CityInfo.API/Controlers/CitiesControlers.cs
@@ -8,17 +8,28 @@ namespace CityInfo.API.Controlers
 {
     [ApiController]
     [Route("api/cities")]
-    public class CitiesControlers:ControllerBase
+    public class CitiesControlers : ControllerBase
     {
         [HttpGet]
-        public JsonResult GetCities()
+        public IActionResult GetCities()
         {
-            return new JsonResult(
-                new List<object>()
-                {
-                    new {id = 1, Name = "New York City"},
-                    new {id = 2, Name = "Antwerp"}
-                });
+            return Ok(CitiesDataStore.Current.Cities);
+            
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCities(int id)
+        {
+            var cityToReturn =  (CitiesDataStore.Current.Cities).FirstOrDefault(x => x.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+            //return new JsonResult((CitiesDataStore.Current.Cities).FirstOrDefault(x => x.Id == id));
+            
         }
     }
 }
