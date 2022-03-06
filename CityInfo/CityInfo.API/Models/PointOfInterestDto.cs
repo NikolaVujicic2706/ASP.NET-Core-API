a62cd9d018fd26afe6bb2ffae85a22c472a31c04
nick_without_wick <nikolavujicic.etf@gmail.com>
2022-03-06 20:29:59 +0100

    PointOfInterestDto class added

--- CityInfo/CityInfo.API/Models/PointOfInterestDto.cs
+++ CityInfo/CityInfo.API/Models/PointOfInterestDto.cs
@@ -5,13 +5,11 @@ using System.Threading.Tasks;
 
 namespace CityInfo.API.Models
 {
-    public class CityDto
+    public class PointOfInterestDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<PointOfInterestDto> PointsOfInterest { get; set; }
-
 
     }
 }
